b47893a093084da709d7e2cc689dd5675db81e03
gonzalorportfolio <93043629+gonzalorportfolio@users.noreply.github.com>
2025-08-22 15:40:23 +0000

    updated to mobile friend and using react-router v7

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Projects';
+import Contact from './pages/Contact';
+import NavBar from './pages/NavBar';
+import './App.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <><NavBar /><Home /></>,
+  },
+  {
+    path: '/about',
+    element: <><NavBar /><About /></>,
+  },
+  {
+    path: '/portfolio',
+    element: <><NavBar /><Portfolio /></>,
+  },
+  {
+    path: '/contact',
+    element: <><NavBar /><Contact /></>,
+  },
+]);
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
---
+++ frontend/src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+const Projects = () => {
+	return (
+		<div style={{padding: '2rem', color: '#222'}}>
+			<h1>Portfolio</h1>
+			<p>This is the Portfolio page.</p>
+		</div>
+	);
+};
+
+export default Projects;
